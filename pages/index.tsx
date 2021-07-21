1b89df1b5abeb30ca15fc7fb940033fec647c263
Alexandru Codreanu <alexandru.codreanu.04@gmail.com>
2021-07-21 12:08:49 +0300

    Added title and favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import Error from '../src/components/error'
+import Head from 'next/head'
 
 const MainPage = () =>
   (
     <div>
+      <Head>
+        <title>Lira Digital Agency</title>
+        <link rel="shortcut icon" href="/liraFavicon.svg"/>
+      </Head>
       <Error></Error>
     </div>
   )
