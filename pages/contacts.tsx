bf3b189449e11b0bc39bd7b3a24fa41bad9bd38c
Alexandru Codreanu <alexandru.codreanu.04@gmail.com>
2021-09-30 10:35:26 +0300

    renaming contacts folder

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Head from 'next/head'
 import Contacts from '../src/components/Contacts/Contacts'
+// ceva aici ne to
 
 const ContactsPage = () =>
   (
