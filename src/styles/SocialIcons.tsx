698c896ff8852ceb0a85042d0afe107c97a10b05
Corneliu <corneliugutu69@gmail.com>
2025-11-25 00:04:45 +0200

    fix: added div container

--- src/styles/SocialIcons.tsx
+++ src/styles/SocialIcons.tsx
@@ -11,43 +11,39 @@ export default function SocialIcons() {
       id="social-links"
       className="flex my-5 ml-3 md:ml-0 md:my-0 md:mr-12 text-sm-h2-poppins md:text-lg-h4-poppins"
     >
-      <Link
-        href={facebookLink}
-        rel="noopener noreferrer"
-        target="_blank"
-        className="mr-5"
-      >
-        <Image
-          src="/footer/Facebook.svg"
-          alt="Facebook"
-          width={24}
-          height={24}
-          className="w-6 h-6"
-        />
-      </Link>
-      <Link
-        href={instagramLink}
-        rel="noopener noreferrer"
-        target="_blank"
-        className="mr-5"
-      >
-        <Image
-          src="/footer/Instagram.svg"
-          alt="Instagram"
-          width={24}
-          height={24}
-          className="w-6 h-6"
-        />
-      </Link>
-      <Link href={linkedinLink} rel="noopener noreferrer" target="_blank">
-        <Image
-          src="/footer/LinkedIn.svg"
-          alt="Linkedin"
-          width={24}
-          height={24}
-          className="w-6 h-6"
-        />
-      </Link>
+      <div className="mr-5">
+        <Link href={facebookLink} rel="noopener noreferrer" target="_blank">
+          <Image
+            src="/footer/Facebook.svg"
+            alt="Facebook"
+            width={24}
+            height={24}
+            className="w-auto h-6"
+          />
+        </Link>
+      </div>
+      <div className="mr-5">
+        <Link href={instagramLink} rel="noopener noreferrer" target="_blank">
+          <Image
+            src="/footer/Instagram.svg"
+            alt="Instagram"
+            width={24}
+            height={24}
+            className="w-auto h-6"
+          />
+        </Link>
+      </div>
+      <div className="mr-5">
+        <Link href={linkedinLink} rel="noopener noreferrer" target="_blank">
+          <Image
+            src="/footer/LinkedIn.svg"
+            alt="Linkedin"
+            width={24}
+            height={24}
+            className="w-auto h-6"
+          />
+        </Link>
+      </div>
     </div>
   )
 }
